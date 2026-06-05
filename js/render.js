297ddb64cf3551ae278eadf600c6aced2750071a
tito <138889363+taitox@users.noreply.github.com>
2026-06-05 03:56:29 +0000

    Update render.js
    admin test

--- js/render.js
+++ js/render.js
@@ -165,6 +165,7 @@ function updateAdminBar() {
   if (S.isAdmin && S.session) {
     setText('admin-email-label', S.session.user.email);
   }
+  renderTVManager();
 }
 
 function renderTableHeaders() {
